efaf845ca1534b681a51da65ed18dde4161b838c
Young Min Shin <holyachon@gmail.com>
2015-03-24 15:15:04 +0900

    add some comments

--- JavaPractice/src/org/dimigo/basic/Condition.java
+++ JavaPractice/src/org/dimigo/basic/Condition.java
@@ -11,12 +11,14 @@ public class Condition {
 
 	public static void main(String[] args) {
 		
+		// initialize variable
 		int fee = 0;
-		int distance = 30; // 거리(단위 : km)
-		int carType = 1; // 1: 고속버스, 2: 경차
+		int distance = 30; // Distance(단위 : km)
+		int carType = 1; // 1: Lowest Bus, 2: Small Car
 		
 		System.out.println("<< 고속도로 통행료 계산 >>");
 		
+		// distance validation
 		if(distance <= 0){
 			System.out.println("거리는 양수만 가능합니다.");
 			return;
@@ -29,22 +31,22 @@ public class Condition {
 		switch(carType){
 		case 1:
 			System.out.println("차종 : 고속버스");
-			fee += 850;
+			fee += 850; // Default Fee of Lowest Bus
 			fee += (int)((distance - 1) / 10) * 300;
 			break;
 		case 2:
 			System.out.println("차종 : 경차");
-			fee += 300;
+			fee += 300; // Default Fee of Small Car
 			fee += (int)((distance - 1) / 10) * 200;
 			break;
 		default:
 			System.out.println("차종 : 그 외");
-			fee += 600;
+			fee += 600; // Default Fee of Others
 			fee += (int)((distance - 1) / 10) * 200;
 		}
 		
+		// print fee
 		System.out.println("통행료 : " + fee + "원");
-		
 
 	}
 
