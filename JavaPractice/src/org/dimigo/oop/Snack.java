42de821bee331b19f3a65df1fd0b0d12855b5ec5
Young Min Shin <holyachon@gmail.com>
2015-05-19 15:32:10 +0900

    10번째 실습과제 제출

---
+++ JavaPractice/src/org/dimigo/oop/Snack.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ Snack
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 5. 19.
+ * </pre>
+ * 
+ * @author		: ym
+ * @version		: 1.0
+ */
+public class Snack {
+	private String name;
+	private String company;
+	private int price;
+	private int number;
+	
+	public Snack(){}
+
+	/**
+	 * @param name
+	 * @param company
+	 * @param price
+	 * @param number
+	 */
+	public Snack(String name, String company, int price, int number) {
+		super();
+		this.name = name;
+		this.company = company;
+		this.price = price;
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	public void printSnack() {
+		System.out.println("이름 : " + this.name);
+		System.out.println("제조사 : " + this.company);
+		System.out.println("가격 : " + String.format("%,d", this.price) + "원");
+		System.out.println("개수 : " + this.number + "개");
+		System.out.println();
+	}
+	
+	public int calcPrice(){
+		return price * number;
+	}
+	
+}
---
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ SnackTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 5. 19.
+ * </pre>
+ * 
+ * @author		: ym
+ * @version		: 1.0
+ */
+public class SnackTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum = 0;
+		
+		Snack[] arrSnack = new Snack[]{
+				new Snack("새우깡", "농심", 1100, 2),
+				new Snack("콘칲", "크라운", 1200, 1),
+				new Snack("허니버터칩", "해태", 1500, 4)
+		};
+		
+		for(Snack value: arrSnack){
+			value.printSnack();
+			
+			sum += value.calcPrice();
+		}
+		
+		System.out.println("총 구매 금액 : " + String.format("%,d", sum) + "원");
+	}
+
+}
