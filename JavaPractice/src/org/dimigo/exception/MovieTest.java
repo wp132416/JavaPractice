b7f38a47d3282bc64076a712097411bc0e1da267
Young Min Shin <holyachon@gmail.com>
2015-09-22 14:39:35 +0900

    for 내부에 try-catch 하게끔 수정

--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -22,17 +22,17 @@ public class MovieTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
-			Movie[] movies = {
-					new Movie("앤트맨", 12), new Movie("사도", 12), new Movie("베테랑", 15)
-			};
-			int age = 13;
-			
-			for(Movie movie : movies) { 
+		Movie[] movies = {
+				new Movie("베테랑", 15), new Movie("앤트맨", 12), new Movie("사도", 12)
+		};
+		int age = 13;
+		
+		for(Movie movie : movies) {
+			try {
 				buyTicket(movie, age);
+			} catch (Exception e){
+				System.out.println(e.getMessage());
 			}
-		} catch (Exception e){
-			System.out.println(e.getMessage());
 		}
 	}
 	
