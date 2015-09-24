db11b0138808a4f14fc0145490adc414bcb2f797
Young Min Shin <holyachon@gmail.com>
2015-09-24 13:54:59 +0900

    실습과제 5 제출

---
+++ JavaPractice/src/org/dimigo/collection/MelonChart.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package org.dimigo.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <pre>
+ * org.dimigo.collection
+ *   |_ MelonChart
+ * 
+ * 1. 개요 : 실습과제 5
+ * 2. 작성일 : 2015. 9. 23.
+ * </pre>
+ * 
+ * @author		: ym
+ * @version		: 1.0
+ */
+public class MelonChart {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Music> list = new ArrayList<Music>();
+		list.add(new Music("바람이나 좀 쐐", "개리"));
+		list.add(new Music("보통연애", "박경"));
+		list.add(new Music("취향저격", "iKON"));
+		
+		System.out.println("-- << 멜론 차트 >> --");
+		printList(list);
+		System.out.println();
+		
+		System.out.println("-- << 2위 곡 추가 >> --");
+		list.add(1, new Music("레옹", "이유갓지"));
+		printList(list);
+		System.out.println();
+		
+		System.out.println("-- << 3위 곡 변경 >> --");
+		list.set(2, new Music("맙소사", "황태지"));
+		printList(list);
+		System.out.println();
+		
+		System.out.println("-- << 4위 곡 삭제 >> --");
+		list.remove(3);
+		printList(list);
+		System.out.println();
+		
+		System.out.println("-- << 전체 리스트 삭제 >> --");
+		list.clear();
+		printList(list);
+	}
+	
+	public static void printList(List<Music> list){
+		int idx = 1;
+		
+		for(Music value : list){
+			System.out.println(idx + ". " + value.toString());
+			idx++;
+		}
+	}
+
+}
