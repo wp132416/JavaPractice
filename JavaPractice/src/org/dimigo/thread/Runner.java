52ae383a406ba34573f387a605c014bb3ff3e217
Young Min Shin <holyachon@gmail.com>
2015-11-04 09:43:27 +0900

    실습과제 9 제출

---
+++ JavaPractice/src/org/dimigo/thread/Runner.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+import java.util.Random;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ *   |_ Runner
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 11. 4.
+ * </pre>
+ * 
+ * @author		: YM
+ * @version		: 1.0
+ */
+public class Runner extends Thread {
+	
+	private String name;
+	
+	public Runner(String name){
+		this.name = name;
+	}
+	
+	@Override
+	public void run() {
+		System.out.println(name + " 출발");
+		
+		for(int i=100; i>0; i=i-10){
+			System.out.println(name + " " + i + " 미터");
+			try {
+				//Thread.sleep(new Random().nextInt(2000));
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println(name + " 골인");
+	}
+}
