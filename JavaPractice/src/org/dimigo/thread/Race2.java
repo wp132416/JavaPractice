2a615a1e7867b51bac85b51d0c7bb7896e1e6b0c
Young Min Shin <holyachon@gmail.com>
2015-11-05 14:03:49 +0900

    실습과제 10 제출

---
+++ JavaPractice/src/org/dimigo/thread/Race2.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.thread;
+
+/**
+ * <pre>
+ * org.dimigo.thread
+ *   |_ Race
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2015. 11. 4.
+ * </pre>
+ * 
+ * @author		: YM
+ * @version		: 1.0
+ */
+public class Race2 {
+	public static void main(String args[]){
+		System.out.println("main method start");
+		
+		Thread r1 = new Thread(new Runner2("정욱재"));
+		Thread r2 = new Thread(new Runner2("박승진"));
+		
+		r1.setPriority(Thread.MAX_PRIORITY);
+		r2.setPriority(Thread.MIN_PRIORITY);
+		
+		r1.start();
+		r2.start();
+		
+		System.out.println("main method end");
+	}
+}
